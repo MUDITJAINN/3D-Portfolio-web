881f00fc26d933fbf3f46a6856d960645035c6fe
Mudit jain <muditj214@gmail.com>
2024-07-27 15:42:56 +0530

    Github profile button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link as LinkR } from "react-router-dom";
+// import { Button } from "react-scroll";
 import styled from "styled-components";
+import { Bio } from "../data/constants";
 
 const NavbarContainer = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -16,6 +18,7 @@ const NavbarContainer = styled.div`
 `;
 
 const NavLogo = styled(LinkR)`
+  width: 80%; 
   padding: 0 6px;
   text-decoration: none;
   color: inherit;
@@ -42,6 +45,37 @@ const NavLink = styled.a`
   }
 `;
 
+const ButtonContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const GithubButton = styled.a`
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  cursor: pointer;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: all 0.6s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
 const Navbar = () => {
   return (
     <NavbarContainer>
@@ -53,6 +87,10 @@ const Navbar = () => {
         <NavLink href="#Projects">Projects</NavLink>
         <NavLink href="#Education">Education</NavLink>
       </NavItems>
+
+      <ButtonContainer>
+        <GithubButton href = {Bio.github} target="_Blank">Github profile</GithubButton>
+      </ButtonContainer>
     </NavbarContainer>
   );
 };
--- src/data/constants.js
+++ src/data/constants.js
@@ -8,13 +8,13 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/MUDITJAINN",
   resume:
     "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  linkedin: "https://www.linkedin.com/in/muditjainn/",
+  twitter: "https://x.com/mudit_jain___",
+  insta: "",
+  facebook: "",
 };
 
 export const skills = [
