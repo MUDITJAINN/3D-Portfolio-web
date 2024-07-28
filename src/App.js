398b2bbcd3e59544fce37a6c0208f330cfff170e
Mudit jain <muditj214@gmail.com>
2024-07-28 19:28:29 +0530

    built Landing page with animated star background, Typewriter effect, and framer-motion animations.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+
+
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
-        <Navbar />
+        <Navbar/>
+        <Hero/>
       </BrowserRouter>
     </ThemeProvider>
   );
